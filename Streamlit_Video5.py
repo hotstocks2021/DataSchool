2f0b8a32511ad9e7b5d90111e985e2d5156ecf71
hotstocks2021 <130184080+hotstocks2021@users.noreply.github.com>
2023-04-20 13:54:08 -0700

    Update Streamlit_Video5.py

--- Streamlit_Video5.py
+++ Streamlit_Video5.py
@@ -3,6 +3,8 @@ import sys
 import streamlit as st
 import pandas as pd
 
+st.write("Home [link](https://hotstocks2021-dataschool-streamlit-dataschool-home-i8njd0.streamlit.app/)")
+
 pd.set_option('expand_frame_repr',False)
 pd.set_option('display.max_colwidth', None)
 # st.set_page_config(layout="wide")
