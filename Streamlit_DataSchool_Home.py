6a035155baabb44ce8c9eea345d7dfa9373c71d0
hotstocks2021 <130184080+hotstocks2021@users.noreply.github.com>
2023-04-20 14:01:13 -0700

    Update Streamlit_DataSchool_Home.py

--- Streamlit_DataSchool_Home.py
+++ Streamlit_DataSchool_Home.py
@@ -10,19 +10,19 @@ st.markdown("""
 ## Data analysis in Python with pandas - Data School 
 \n https://www.youtube.com/watch?v=yzIMircGU5I&list=PL5-da3qGB5ICCsgW1MxlZ0Hq8LL5U3u9y
 
-Video 2: How do I read a tabular data file into pandas?
+Video 2: How do I read a :red[tabular data file] into pandas?
 \nhttps://hotstocks2021-dataschool-streamlit-video2-lbi26f.streamlit.app/
 \n
-Video 3: How do I select a Pandas Series from a DataFrame?
+Video 3: How do I select a :red[Pandas Series] from a DataFrame?
 \nhttps://hotstocks2021-dataschool-streamlit-video3-8efh30.streamlit.app/
 \n
-Video 4: Pandas Attribute and Method
+Video 4: Pandas :red[Attribute and Method]
 \nhttps://hotstocks2021-dataschool-streamlit-video4-gcszbr.streamlit.app/
 \n
-Video 5: How do I rename columns in a pandas DataFrame?
+Video 5: How do I :red[rename columns] in a pandas DataFrame?
 \nhttps://hotstocks2021-dataschool-streamlit-video5-ozwrhd.streamlit.app/
 \n
-Video 6: How do I remove columns from a pandas DataFrame?
+Video 6: How do I :red[remove columns] from a pandas DataFrame?
 \nhttps://hotstocks2021-dataschool-streamlit-video6-jzo273.streamlit.app/
 \n
 """
