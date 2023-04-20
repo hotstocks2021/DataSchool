6d70a6d93b5431b0769ed5aceb708b6da17f445b
hotstocks2021 <130184080+hotstocks2021@users.noreply.github.com>
2023-04-20 13:39:46 -0700

    Add files via upload

---
+++ Streamlit_DataSchool_Home.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import streamlit as st
+import pandas as pd
+
+pd.set_option('expand_frame_repr',False)
+pd.set_option('display.max_colwidth', None)
+# st.set_page_config(layout="wide")
+st.markdown("""
+## Data analysis in Python with pandas - Data School 
+\n https://www.youtube.com/watch?v=yzIMircGU5I&list=PL5-da3qGB5ICCsgW1MxlZ0Hq8LL5U3u9y
+
+## Set the display options
+:red[pd.set_option('expand_frame_repr',False)]
+\n:red[pd.set_option('display.max_colwidth', None)]
+
+## Video 2: How do I read a tabular data file into pandas?
+\nhttps://hotstocks2021-dataschool-streamlit-video2-lbi26f.streamlit.app/
+\n
+## Video 3: How do I select a Pandas Series from a DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video3-8efh30.streamlit.app/
+\n
+## Video 4: Pandas Attribute and Method
+\nhttps://hotstocks2021-dataschool-streamlit-video4-gcszbr.streamlit.app/
+\n
+## Video 5: How do I rename columns in a pandas DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video5-ozwrhd.streamlit.app/
+\n
+## Video 6: How do I remove columns from a pandas DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video6-jzo273.streamlit.app/
+\n
+"""
+)
