f5fdcac6b861cd6a507682a800b5edcb0770cd3e
hotstocks2021 <130184080+hotstocks2021@users.noreply.github.com>
2023-04-25 13:26:14 -0700

    Update Streamlit_DataSchool_Home.py

--- Streamlit_DataSchool_Home.py
+++ Streamlit_DataSchool_Home.py
@@ -37,3 +37,46 @@ Video 9: How do I :red[apply multiple filter criteria] to a pandas DataFrame?
 """
 )
 
+import os
+import sys
+import streamlit as st
+import pandas as pd
+
+pd.set_option('expand_frame_repr',False)
+pd.set_option('display.max_colwidth', None)
+# st.set_page_config(layout="wide")
+st.markdown("""
+## Data analysis in Python with pandas - Data School 
+\n https://www.youtube.com/watch?v=yzIMircGU5I&list=PL5-da3qGB5ICCsgW1MxlZ0Hq8LL5U3u9y
+
+Video 2: How do I read a :red[tabular data file] into pandas?
+\nhttps://hotstocks2021-dataschool-streamlit-video2-lbi26f.streamlit.app/
+\n
+Video 3: How do I select a :red[Pandas Series] from a DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video3-8efh30.streamlit.app/
+\n
+Video 4: Pandas :red[Attribute and Method]
+\nhttps://hotstocks2021-dataschool-streamlit-video4-gcszbr.streamlit.app/
+\n
+Video 5: How do I :red[rename columns] in a pandas DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video5-ozwrhd.streamlit.app/
+\n
+Video 6: How do I :red[remove columns] from a pandas DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video6-jzo273.streamlit.app/
+\n
+Video 7: How do I :red[sort] a pandas DataFrame or a Series?
+\nhttps://hotstocks2021-dataschool-streamlit-video7-1djh6g.streamlit.app/
+\n
+Video 8: How do I :red[filter rows] of a pandas DataFrame by column value?
+\nhttps://hotstocks2021-dataschool-streamlit-video8-z6nkbu.streamlit.app/
+\n
+Video 9: How do I :red[apply multiple filter criteria] to a pandas DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video9-a6hnvz.streamlit.app/
+\n
+Video 10: How do I :red[apply multiple filter criteria] to a pandas DataFrame?
+\nhttps://hotstocks2021-dataschool-streamlit-video10-hjpo76.streamlit.app/
+\n
+"""
+)
+
+
