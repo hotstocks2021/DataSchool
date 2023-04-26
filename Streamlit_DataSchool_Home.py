d58afed5a66cc9dbf1e883247d8227963659a616
hotstocks2021 <130184080+hotstocks2021@users.noreply.github.com>
2023-04-26 07:04:02 -0700

    Update Streamlit_DataSchool_Home.py

--- Streamlit_DataSchool_Home.py
+++ Streamlit_DataSchool_Home.py
@@ -10,28 +10,36 @@ st.markdown("""
 ## Data analysis in Python with pandas - Data School 
 \n https://www.youtube.com/watch?v=yzIMircGU5I&list=PL5-da3qGB5ICCsgW1MxlZ0Hq8LL5U3u9y
 
-Video 2: How do I read a :red[tabular data file] into pandas?
+Video 2: 
+- How do I read a :red[tabular data file] into pandas?
 \nhttps://hotstocks2021-dataschool-streamlit-video2-lbi26f.streamlit.app/
 \n
-Video 3: How do I select a :red[Pandas Series] from a DataFrame?
+Video 3: 
+- How do I select a :red[Pandas Series] from a DataFrame?
 \nhttps://hotstocks2021-dataschool-streamlit-video3-8efh30.streamlit.app/
 \n
-Video 4: Pandas :red[Attribute and Method]
+Video 4: 
+- Pandas :red[Attribute and Method]
 \nhttps://hotstocks2021-dataschool-streamlit-video4-gcszbr.streamlit.app/
 \n
-Video 5: How do I :red[rename columns] in a pandas DataFrame?
+Video 5: 
+- How do I :red[rename columns] in a pandas DataFrame?
 \nhttps://hotstocks2021-dataschool-streamlit-video5-ozwrhd.streamlit.app/
 \n
-Video 6: How do I :red[remove columns] from a pandas DataFrame?
+Video 6: 
+- How do I :red[remove columns] from a pandas DataFrame?
 \nhttps://hotstocks2021-dataschool-streamlit-video6-jzo273.streamlit.app/
 \n
-Video 7: How do I :red[sort] a pandas DataFrame or a Series?
+Video 7: 
+- How do I :red[sort] a pandas DataFrame or a Series?
 \nhttps://hotstocks2021-dataschool-streamlit-video7-1djh6g.streamlit.app/
 \n
-Video 8: How do I :red[filter rows] of a pandas DataFrame by column value?
+Video 8: 
+- How do I :red[filter rows] of a pandas DataFrame by column value?
 \nhttps://hotstocks2021-dataschool-streamlit-video8-z6nkbu.streamlit.app/
 \n
-Video 9: How do I :red[apply multiple filter criteria] to a pandas DataFrame?
+Video 9:
+- How do I :red[apply multiple filter criteria] to a pandas DataFrame?
 \nhttps://hotstocks2021-dataschool-streamlit-video9-a6hnvz.streamlit.app/
 \n
 Video 10: 
@@ -39,6 +47,6 @@ Video 10:
 - Read only specific rows from a DataFrame
 - Iterate DataFrame
 - Select only numeric columns from a DataFrame
-\nhttps://hotstocks2021-dataschool-streamlit-video9-a6hnvz.streamlit.app/
+\nhttps://hotstocks2021-dataschool-streamlit-video10-hjpo76.streamlit.app/
 """
 )
