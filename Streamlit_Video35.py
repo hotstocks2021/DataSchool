72d00b0e87bba69afb4a6beed6a06b126e4e2765
hotstocks2021 <130184080+hotstocks2021@users.noreply.github.com>
2023-05-20 11:59:25 -0700

    Update Streamlit_Video35.py

--- Streamlit_Video35.py
+++ Streamlit_Video35.py
@@ -173,8 +173,8 @@ st.table(stocks.head())
 format_dict = {'Date':'{:%m/%d/%y}', 'Close':'${:.2f}', 'Volume':'{:,}'}
 st.table(stocks.style.format(format_dict))
 
-st.table(stocks.style.format(format_dict).hide_index().highlight_min('Close', color='red').highlight_max('Close', color='lightgreen'))
+st.table(stocks.style.format(format_dict).hide().highlight_min('Close', color='red').highlight_max('Close', color='lightgreen'))
 
-st.table(stocks.style.format(format_dict).hide_index().background_gradient(subset='Volume', cmap='Blues'))
+st.table(stocks.style.format(format_dict).hide().background_gradient(subset='Volume', cmap='Blues'))
 
-st.table(stocks.style.format(format_dict).hide_index().bar('Volume', color='lightblue', align='zero').set_caption('Stock Prices from October 2016'))
+st.table(stocks.style.format(format_dict).hide().bar('Volume', color='lightblue', align='zero').set_caption('Stock Prices from October 2016'))
